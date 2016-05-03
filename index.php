6ed14c6a540345ab96917efaf1cd9d538bd23d4c
alexandrefm <zedmaster@gmail.com>
2016-05-03 13:31:09 -0300

    * Adicionado o icone do site

--- index.php
+++ index.php
@@ -65,7 +65,8 @@ if(strlen($_FILES['userfile']['tmp_name']) > 0){
 <html>
 <head>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-<title>Caguei</title>
+    <link rel=icon href=favicon.ico>
+    <title>Caguei</title>
 </head>
 <body>
     <div class="page-header">
